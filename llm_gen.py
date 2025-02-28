849af6c66249b3a58ff4455eadc0de6128259aa0
吴子豪 <vortez.wohl@gmail.com>
2025-02-28 13:51:16 +0800

    update

---
+++ llm_gen.py
@@ -0,0 +1,30 @@
+import json
+
+from ceo import get_openai_model
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+model = get_openai_model(temp=0.9, top_p=0.7)
+
+data = {
+    'prompt': [],
+    'human_result': [],
+    'llm_result': []
+}
+
+with open('./script_review_alpaca_2025-02-28-13-47-13.json', 'r', encoding='utf-8') as f:
+    data_json = json.load(f)
+
+for i, item in enumerate(data_json):
+    prompt = item['instruction'] + item['input']
+    human_result = item['output']
+    llm_result = model.invoke(prompt).content
+    data['prompt'].append(prompt)
+    data['human_result'].append(human_result)
+    data['llm_result'].append(llm_result)
+    print(f'Round {i+1}')
+    print('Prompt:', prompt)
+    print('Response:', llm_result, end='\n\n')
+    df = pd.DataFrame(data)
+    df.to_csv('./test_result/gpt4o-mini.csv', index=False, encoding='utf-8')
